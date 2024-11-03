1ebbcd49829c4ce6e3d6c769ed080b88356b1198
soramon0 <23435312+soramon0@users.noreply.github.com>
2024-11-03 22:34:16 +0100

    fix formatting

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -14,3 +14,4 @@ int	ft_isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
+
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -23,9 +23,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	while (haystack[i] && i < len)
 	{
 		j = 0;
-		while (haystack[i + j] && needle[j] && haystack[i + j] == needle[j]) {
+		while (haystack[i + j] && needle[j] && haystack[i + j] == needle[j])
 			j++;
-		}
 		if (needle[j] == '\0')
 			return ((char *)&haystack[i]);
 		if (j > 0)
