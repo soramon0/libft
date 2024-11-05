bf30ef31f819457f8af129134270fcefa8b26520
soramon0 <23435312+soramon0@users.noreply.github.com>
2024-11-05 14:08:50 +0100

    fix header formatting

--- libft.h
+++ libft.h
@@ -6,12 +6,12 @@
 /*   By: klaayoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 22:18:11 by klaayoun          #+#    #+#             */
-/*   Updated: 2024/10/25 22:21:05 by klaayoun         ###   ########.fr       */
+/*   Updated: 2024/11/05 14:07:37 by klaayoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIB_FT_H
-# define LIB_FT_H
+#ifndef LIBFT_H
+# define LIBFT_H
 
 # include <stddef.h>
 
@@ -24,9 +24,9 @@ int	ft_isprint(int c);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
 int	ft_strlen(const char *s);
-char  *ft_strchr(const char *s, int c);
-char  *ft_strrchr(const char *s, int c);
-int ft_strncmp(const char *s1, const char *s2, size_t n);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 #endif
